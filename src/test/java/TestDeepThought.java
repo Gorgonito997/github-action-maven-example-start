d0149f7f803c046e6579415f2b6242ff6cc9edb2
Ca(n)sado <diegocm505@gmail.com>
2025-03-11 09:48:55 +0000

    Cambios antes de hacer el job

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -3,16 +3,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-public class TestDeepThought {
-    public TestDeepThought() {
+public class TestRedisJava {
+    public TestRedisJava() {
 
     }
 
     @Test
-    @DisplayName("Test the answer to everything")
-    void testAnswerToAll() {
-        DeepThought superComputer = new DeepThought();
-        assertEquals(42, superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
-    }
+    @DisplayName("Test Redis cache")
+    void testRedisCache() {
+        DeepThought ourSuperComputer = new DeepThought();
+        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
 
+        RedisJava cache = new RedisJava();
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything", String.valueOf(ans));
+
+        assertEquals("42",
+                cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
+    }
 }
